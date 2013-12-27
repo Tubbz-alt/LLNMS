dcb9114f279a10119f2c3262876cd4c5ec9e7d4e
Marvin Smith <marvin_smith1@me.com>
2013-12-26 16:55:36 -0800

    LLNMS Remove Asset works by searching by hostname and ip4 address.

--- src/bash/assets/llnms-remove-asset.sh
+++ src/bash/assets/llnms-remove-asset.sh
@@ -7,5 +7,270 @@
 #   Purpose:  Removes an LLNMS asset if it exists
 #
 
+#----------------------------------------#
+#-       Print Usage Instructions       -#
+#----------------------------------------#
+usage(){
+
+    echo "`basename $0` [options]"
+    echo ''
+    echo '    options:'
+    echo '        -h, --help        :  Print usage instructions'
+    echo '        -v, --version     :  Print version information'
+    echo '        -i, --interactive :  Give warnings before removal.'
+    echo ''
+    echo '        Search Options:  (Search using one or more options.)'
+    echo '        -host, --hostname [hostname]  : Search for asset by hostname'
+    echo '        -ip4, --ip4-address [address] : Search for asset by ip4 address'
+    echo ''
+}
+
+
+#-------------------------------------#
+#-         Warning Function          -#
+#-                                   -#
+#-   $1 -  Error Message             -#
+#-   $2 -  Line Number (Optional).   -#
+#-   $3 -  File Name (Optional).     -$
+#-------------------------------------#
+warning(){
+
+    #  If the user only gives the warning message
+    if [ $# -eq 1 ]; then
+        echo "warning: $1"
+
+    #  If the user only gives the line number
+    elif [ $# -eq 2 ]; then
+        echo "warning: $1  Line: $2,  File: `basename $0`"
+
+    #  If the user gives the line number and file
+    else
+        echo "warning: $1  Line: $2, File: $3"
+    fi
+}
+
+#-------------------------------------#
+#-            Error Function         -#
+#-                                   -#
+#-   $1 -  Error Message             -#
+#-   $2 -  Line Number (Optional).   -#
+#-   $3 -  File Name (Optional).     -$
+#-------------------------------------#
+error(){
+
+    #  If the user only gives the error message
+    if [ $# -eq 1 ]; then
+        echo "error: $1"
+
+    #  If the user only gives the line number
+    elif [ $# -eq 2 ]; then
+        echo "error: $1  Line: $2,  File: `basename $0`"
+
+    #  If the user gives the line number and file
+    else
+        echo "error: $1  Line: $2, File: $3"
+    fi
+}
+
+
+#-------------------------------------#
+#           Version Function          #
+#-------------------------------------#
+version(){
+
+    echo "`basename $0` Information"
+    echo ''
+    echo "   LLNMS Version ${LLNMS_MAJOR}.${LLNMS_MINOR}.${LLNMS_SUBMINOR}"
+
+}
+
+ 
+#------------------------------------------------------------#
+#-        Delete the file given the interactive state       -#
+#-             and the file to delete.                      -#
+#-                                                          -#
+#-      $1 - Interactive Mode State: 0 - Off, 1 - On        -#
+#-      $2 - File to delete                                 -#
+#-      $3 - Matched Pattern                                -#
+#------------------------------------------------------------#
+discard_file(){
+
+    INTERACTIVE_FLAG="$1"
+    ASSET_FILE="$2"
+    REMOVE_FILE=0
+    
+    #  If interactive mode is set, then print the warning
+    if [ "$INTERACTIVE_FLAG" = '1' ]; then
+        
+        echo "File $ASSET_FILE found which matches the pattern..."
+        echo "   $3"
+        echo "Do you wish to delete the file? (y/n - default): \c"
+        read ANS
+        
+        #  If the user wants to delete the file, then set the flag
+        if [ "$ANS" = 'y' -o "$ANS" = 'Y' ]; then
+            echo ''
+            echo 'Removing File'
+            REMOVE_FILE=1
+
+        else
+            echo 'Skipping file'
+        fi
+        echo ''
+    
+    #  Otherwise, if the user is not in interactive mode, then just delete the file
+    else
+        REMOVE_FILE=1
+    fi
+    
+    if [ "$REMOVE_FILE" = "1" ]; then
+        rm $ASSET_FILE
+    fi
+
+}
+
+
+#---------------------------------#
+#-         Main Function         -#
+#---------------------------------#
+
+#  Source llnms home
+if [ "$LLNMS_HOME" = "" ]; then
+    LLNMS_HOME="/var/tmp/llnms"
+fi
+
+
+#  Import the version info
+. $LLNMS_HOME/config/llnms-info.sh
+
+#  Import configuration info
+. $LLNMS_HOME/config/llnms-config.sh
+
+#   Search flags and values
+SEARCH_HOSTNAME=''
+HOSTNAME_FLAG=0
+
+SEARCH_IP4ADDRESS=''
+IP4ADDRESS_FLAG=0
+
+INTERACTIVE_FLAG=0
+
+
+#   Parse Command-Line Options
+for OPTION in "$@"; do
+
+    case $OPTION in
+
+        #  Print usage instructions
+        '-h' | '--help' )
+            usage
+            exit 1
+            ;;
+
+
+        #  Print version information
+        '-v' | '--version' )
+            version
+            exit 1
+            ;;
+
+        #  Search by hostname
+        '-host' | '--hostname' )
+            HOSTNAME_FLAG=1
+            ;;
+        
+        #  Search by ip4 address
+        '-ip4' | '--ip4-address' )
+            IP4ADDRESS_FLAG=1
+            ;;
+
+        #  Interactive flag
+        '-i' | '--interactive' )
+            INTERACTIVE_FLAG=1
+            ;;
+
+        #  Process flag values or print error message
+        *)
+            
+            #  If the user wants to search by hostname
+            if [ $HOSTNAME_FLAG -eq 1 ]; then
+                HOSTNAME_FLAG=0
+                SEARCH_HOSTNAME=$OPTION
+
+            #  If the user wants to search by ip4 address
+            elif [ $IP4ADDRESS_FLAG -eq 1 ]; then
+                IP4ADDRESS_FLAG=0
+                SEARCH_IP4ADDRESS=$OPTION
+
+            #  Print an error for an unknown option
+            else
+                error "Unknown option $OPTION"
+                usage
+                exit 1
+            fi
+
+            ;;
+
+
+    esac
+done
+
+
+#-------------------------------------------------------------------#
+#-     Make sure there is something to search for an asset by      -#
+#-------------------------------------------------------------------#
+if [ "$SEARCH_HOSTNAME" == '' -a "$SEARCH_IP4ADDRESS" == '' ]; then
+    error "In order to remove asset, you must provide either hostname or ip4 address."
+    exit 1
+fi
+ 
+
+#--------------------------------------#
+#-      Get a list of assets          -#
+#--------------------------------------#
+
+#  Flag which tells us if anything was found
+ASSET_MATCH_FOUND=0
+
+ASSET_FILES="`ls $LLNMS_HOME/assets/*.llnms-asset.xml 2> /dev/null`"
+for ASSET_FILE in $ASSET_FILES; do
+    
+    #  If we are searching using hostname, then grab the hostname
+    if [ ! "$SEARCH_HOSTNAME" = '' ]; then
+        TEMP_HOSTNAME="`llnms-print-asset-info.sh -f $ASSET_FILE -host`"
+       
+        #  If the hostname of the file and our search hostname match, then remove the file
+        if [ "$TEMP_HOSTNAME" = "$SEARCH_HOSTNAME" ]; then
+            discard_file $INTERACTIVE_FLAG $ASSET_FILE "hostname: $SEARCH_HOSTNAME"
+            ASSET_MATCH_FOUND=1
+        fi
+    fi
+
+    #  If the pattern matches additional criterion and has been deleted, go no futher
+    SKIP_ASSET=0
+    if [ ! -e "$ASSET_FILE" ]; then
+        SKIP_ASSET=1
+    fi
+
+    #  If we are searching using IP4 Address, then grab the address
+    if [ "$SKIP_ASSET" = 0 -a ! "$SEARCH_IP4ADDRESS" = "" ]; then
+        TEMP_IP4ADDRESS="`llnms-print-asset-info.sh -f $ASSET_FILE -ip4`"
+        
+        #  If the address of the file and our search address match, then remove the file
+        if [ "$TEMP_IP4ADDRESS" = "$SEARCH_IP4ADDRESS" ]; then
+            discard_file $INTERACTIVE_FLAG $ASSET_FILE "ip4-address: $SEARCH_IP4ADDRESS"
+            ASSET_MATCH_FOUND=1
+        fi
+    
+    fi
+
+done
+
+#  If no match was found, inform the user
+if [ "$ASSET_MATCH_FOUND" = "0" ]; then
+    error 'No match was found with the input search criteria' "$LINENO"
+    exit 1
+fi
+
 
 
--- test/assets/TEST_llnms_remove_asset.sh
+++ test/assets/TEST_llnms_remove_asset.sh
@@ -8,10 +8,128 @@
 #
 
 
-TEST_llnms_remove_asset_01(){
+#  Make sure LLNMS has been installed
+if [ "$LLNMS_HOME" = "" ]; then
+    LLNMS_HOME="/var/tmp/llnms"
+fi
 
-    echo '1'
-    echo 'Not Implemented' > /var/tmp/cause.txt
+#  Import llnms configuration
+. $LLNMS_HOME/config/llnms-config.sh
+
+# Initialize ANSI
+. test/unit_test/unit_test_utilities.sh
+ 
+
+#------------------------------------------------#
+#-     Helper function for creating assets      -#
+#-        for testing use only.                 -#
+#------------------------------------------------#
+TMP_llnms_remove_asset_create_assets(){
+
+    #  Create 3 temporary assets
+    llnms-create-asset.sh  -host 'temp-asset1' -ip4 '192.168.0.1' -d 'hello world'
+    llnms-create-asset.sh  -host 'temp-asset2' -ip4 '10.2.10.1'   -d 'sample file'
+    llnms-create-asset.sh  -host 'temp-asset3' -ip4 '172.16.0.1'  -d 'final test file'
 
 }
+
+
+#--------------------------------------------------------#
+#-      Helper function for testing if certain assets   -#
+#-         still exist or were deleted.                 -#
+#-                                                      -#
+#-      $1 - temp-asset1 state, 0 - deleted, 1 - exists -#
+#-      $2 - temp-asset2 state, 0 - deleted, 1 - exists -#
+#-      $3 - temp-asset3 state, 0 - deleted, 1 - exists -#
+#--------------------------------------------------------#
+TMP_llnms_remove_asset_test_assets(){
+
+    #  If temp-asset1 exists
+    if [ -e "$LLNMS_HOME/assets/temp-asset1.llnms-asset.xml" ]; then 
+        if [ $1 = '0' ]; then
+            echo "Asset 'temp-asset1' exists where it should not.  File: `basename $0`, Line: $LINENO" > /var/tmp/cause.txt
+            echo '1'
+            return
+        fi
+    #  If temp-asset1 does not exist
+    else 
+        if [ $1 = '1' ]; then
+            echo " Asset 'temp-asset1' does not exists when it should.  File: `basename $0`, Line $LINENO" > /var/tmp/cause.txt
+            echo '1'
+            return
+        fi
+    fi
+    
+    #  If temp-asset2 exists
+    if [ -e "$LLNMS_HOME/assets/temp-asset2.llnms-asset.xml" ]; then 
+        if [ $2 = '0' ]; then
+            echo "Asset 'temp-asset2' exists where it should not.  File: `basename $0`, Line: $LINENO" > /var/tmp/cause.txt
+            echo '1'
+            return
+        fi
+    #  If temp-asset2 does not exist
+    else 
+        if [ $2 = '1' ]; then
+            echo " Asset 'temp-asset2' does not exists when it should.  File: `basename $0`, Line $LINENO" > /var/tmp/cause.txt
+            echo '1'
+            return
+        fi
+    fi
+    
+    #  If temp-asset3 exists
+    if [ -e "$LLNMS_HOME/assets/temp-asset3.llnms-asset.xml" ]; then 
+        if [ $3 = '0' ]; then
+            echo "Asset 'temp-asset3' exists where it should not.  File: `basename $0`, Line: $LINENO" > /var/tmp/cause.txt
+            echo '1'
+            return
+        fi
+    #  If temp-asset3 does not exist
+    else 
+        if [ $3 = '1' ]; then
+            echo " Asset 'temp-asset3' does not exists when it should.  File: `basename $0`, Line $LINENO" > /var/tmp/cause.txt
+            echo '1'
+            return
+        fi
+    fi
+
+    echo "0"
+}
+
+
+#------------------------------------------------#
+#-     TEST_llnms_remove_asset_01               -#
+#-                                              -#
+#-     Test the remove asset script from the    -#
+#-     llnms-remove-asset script.               -#
+#------------------------------------------------#
+TEST_llnms_remove_asset_01(){
+    
+    #  Make sure removals happen regardless
+    rm -r "$LLNMS_HOME/assets/*.llnms-asset.xml" 2> /dev/null
+    
+    #  Create temp assets
+    TMP_llnms_remove_asset_create_assets
+    
+    #  Remove the temp-asset1 asset by hostname
+    llnms-remove-asset.sh -host 'temp-asset1'
+    
+    #  Test asset existence
+    RESULT="`TMP_llnms_remove_asset_test_assets 0 1 1`"
+    if [ "$RESULT" = '1' ]; then echo '1'; return; fi
+   
+    #  Remove the temp-asset3 by address
+    llnms-remove-asset.sh -ip4 '172.16.0.1'
+    
+    #  Test asset existence
+    RESULT="`TMP_llnms_remove_asset_test_assets 0 1 0`"
+    if [ "$RESULT" = '1' ]; then echo '1'; return; fi
+    
+    #  Make sure everything has been removed anyhow
+    rm -r "$LLNMS_HOME/assets/*.llnms-asset.xml" 2> /dev/null
+
+    #  Return with successful operation
+    echo '0'
+
+}
+
 
