26f9108b27b1d252225de5b1db2993422e0f7bd3
Marvin Smith <marvin_smith1@me.com>
2013-12-26 21:31:28 -0800

    adding code to print scanner information and adding unit tests

---
+++ src/bash/scanning/llnms-print-scanner-info.sh
@@ -0,0 +1,258 @@
+#!/bin/sh
+#
+#   File:    llnms-print-scanner-info.sh
+#   Author:  Marvin Smith
+#   Date:    12/26/2013
+#
+#   Purpose:  Print information about an LLNMS Scanner.
+#
+
+
+#----------------------------------------#
+#-       Print Usage Instructions       -#
+#----------------------------------------#
+usage(){
+
+    echo "`basename $0` [options] -f <scanner-file>"
+    echo ''
+    echo '    Options:'
+    echo '        -h, --help         :  Print usage instructions'
+    echo '        -v, --version      :  Print version information'
+    echo '        -f, --file [*.llnms-scanner.xml file] :  Set the LLNMS Scanner File to read.'
+    echo ''
+    echo '    Printing Options (Select one or more of the following)'
+    echo '        -a, --all          :  Print everything (Default)'
+    echo '        -i, --id           :  Print scanner id'
+    echo '        -n, --name         :  Print scanner name'
+    echo '        -d, --description  :  Print scanner description.'
+    echo '        -c, --command      :  Print scanner command.'
+    echo ''
+
+}
+
+
+#-------------------------------------#
+#-         Warning Function          -#
+#-                                   -#
+#-   $1 -  Error Message             -#
+#-   $2 -  Line Number (Optional).   -#
+#-   $3 -  File Name (Optional).     -$
+#-------------------------------------#
+warning(){
+
+    #  If the user only gives the warning message
+    if [ $# -eq 1 ]; then
+        echo "warning: $1."
+
+    #  If the user only gives the line number
+    elif [ $# -eq 2 ]; then
+        echo "warning: $1.  Line: $2,  File: `basename $0`"
+
+    #  If the user gives the line number and file
+    else
+        echo "warning: $1.  Line: $2, File: $3"
+    fi
+}
+
+#-------------------------------------#
+#-            Error Function         -#
+#-                                   -#
+#-   $1 -  Error Message             -#
+#-   $2 -  Line Number (Optional).   -#
+#-   $3 -  File Name (Optional).     -$
+#-------------------------------------#
+error(){
+
+    #  If the user only gives the error message
+    if [ $# -eq 1 ]; then
+        echo "error: $1."
+
+    #  If the user only gives the line number
+    elif [ $# -eq 2 ]; then
+        echo "error: $1.  Line: $2,  File: `basename $0`"
+
+    #  If the user gives the line number and file
+    else
+        echo "error: $1.  Line: $2, File: $3"
+    fi
+}
+
+
+#-------------------------------------#
+#           Version Function          #
+#-------------------------------------#
+version(){
+
+    echo "`basename $0` Information"
+    echo ''
+    echo "   LLNMS Version ${LLNMS_MAJOR}.${LLNMS_MINOR}.${LLNMS_SUBMINOR}"
+
+}
+
+
+#---------------------------------#
+#-         Main Function         -#
+#---------------------------------#
+
+#  Source llnms home
+if [ "$LLNMS_HOME" = "" ]; then
+    LLNMS_HOME="/var/tmp/llnms"
+fi
+
+
+#  Import the version info
+. $LLNMS_HOME/config/llnms-info.sh
+
+#  Import the configuration info
+. $LLNMS_HOME/config/llnms-config.sh
+
+#  Important flags
+FILE_FLAG=0
+FILE_VALUE=''
+
+PRINT_EVERYTHING=1
+PRINT_ID=0
+PRINT_NAME=0
+PRINT_DESCRIPTION=0
+PRINT_COMMAND=0
+
+#   Parse Command-Line Options
+for OPTION in "$@"; do
+
+    case $OPTION in
+
+        #  Print usage instructions
+        '-h' | '--help' )
+            usage
+            exit 1
+            ;;
+
+
+        #  Print version information
+        '-v' | '--version' )
+            version
+            exit 1
+            ;;
+
+        #  Set File Flag
+        '-f' | '--file' )
+            FILE_FLAG=1
+            ;;
+
+        #  Print everything
+        '-a' | '--all' )
+            PRINT_EVERYTHING=1
+            ;;
+
+        #  Print id
+        '-i' | '--id' )
+            PRINT_EVERYTHING=0
+            PRINT_ID=1
+            ;;
+
+        #  Print name
+        '-n' | '--name' )
+            PRINT_EVERYTHING=0
+            PRINT_NAME=1
+            ;;
+
+        #  Print description
+        '-d' | '--description' )
+            PRINT_EVERYTHING=0
+            PRINT_DESCRIPTION=1
+            ;;
+
+        #  Print command
+        '-c' | '--command' )
+            PRINT_EVERYTHING=0
+            PRINT_COMMAND=1
+            ;;
+        
+        #  Process flag values or print error message
+        *)
+            
+            #  If file flag set, then grab the filename
+            if [ "$FILE_FLAG" = '1' ]; then
+                FILE_FLAG=0
+                FILE_VALUE=$OPTION
+
+            #  Otherwise, throw an error for an unknown option
+            else
+                error "Unknown option $OPTION" "$LINENO"
+                usage
+                exit 1
+            fi
+            ;;
+
+
+    esac
+done
+
+
+#-----------------------------------------------#
+#-      Make sure the filename has been set    -#
+#-----------------------------------------------#
+if [ "$FILE_VALUE" = "" ]; then
+    error "No filename set.  Use the -f flag." "$LINENO"
+    usage
+    exit 1
+fi
+
+#--------------------------------------#
+#-      Make sure the file exists     -#
+#--------------------------------------#
+if [ ! -e "$FILE_VALUE" ]; then
+    error "$FILE_VALUE does not exist." "$LINENO"
+    usage
+    exit 1
+fi
+
+#----------------------------------------------------------------#
+#-      Make sure the scanner file has the proper extension.    -#
+#----------------------------------------------------------------#
+if [ "`echo $FILE_VALUE | sed -n '/.*\.llnms-scanner\.xml/ p'`" = '' ]; then
+    warning "File ($FILE_VALUE) does not have the proper .llnms-scanner.xml extension." "$LINENO" "`basename $0`"
+fi
+
+
+#------------------------------------------------#
+#-   Start printing the required information    -#
+#------------------------------------------------#
+DATA_PRINTED=0
+
+#  Print id
+if [ "$PRINT_ID" = '1' -o "$PRINT_EVERYTHING" = '1' ]; then
+    $ECHO "`xmlstarlet sel -t -m '//llnms-scanner' -v 'id' -n $FILE_VALUE`\c"
+    DATA_PRINTED=1
+fi
+
+#  Print name
+if [ "$PRINT_NAME" = '1' -o "$PRINT_EVERYTHING" = '1' ]; then
+    if [ "$DATA_PRINTED" = '1' ]; then
+        echo ", \c"
+    fi
+    $ECHO "`xmlstarlet sel -t -m '//llnms-scanner' -v 'name' -n $FILE_VALUE`\c"
+    DATA_PRINTED=1
+fi
+
+#  Print description
+if [ "$PRINT_DESCRIPTION" = '1' -o "$PRINT_EVERYTHING" = '1' ]; then
+    if [ "$DATA_PRINTED" = '1' ]; then
+        echo ", \c"
+    fi
+    $ECHO "`xmlstarlet sel -t -m '//llnms-scanner' -v 'description' -n $FILE_VALUE`\c"
+    DATA_PRINTED=1
+fi
+
+#  Print Command
+if [ "$PRINT_COMMAND" = '1' -o "$PRINT_EVERYTHING" = '1' ]; then
+    if [ "$DATA_PRINTED" = '1' ]; then
+        echo ", \c"
+    fi
+    $ECHO "`xmlstarlet sel -t -m '//llnms-scanner/configuration/linux' -v 'command' -n $FILE_VALUE`\c"
+    DATA_PRINTED=1
+fi
+
+#  print final newline
+echo ''
+
---
+++ test/scanning/TEST_llnms_print_scanner_info.sh
@@ -0,0 +1,52 @@
+#!/bin/sh
+#
+#   File:    TEST_llnms_print_scanner_info.sh
+#   Author:  Marvin Smith
+#   Date:    12/26/2013
+#
+#   Purpose:  This contains all tests related to the llnms-print-scanner-info script.
+#
+
+
+#  Make sure LLNMS has been installed
+if [ "$LLNMS_HOME" = "" ]; then
+    LLNMS_HOME="/var/tmp/llnms"
+fi
+
+#  Import llnms configuration
+. $LLNMS_HOME/config/llnms-config.sh
+
+# Initialize ANSI
+. test/unit_test/unit_test_utilities.sh
+
+
+#---------------------------------------#
+#-   TEST_llnms_print_scanner_info_01  -#
+#-                                     -#
+#-   Test the created file output      -#
+#-   from llnms-print-scanner-info.    -#
+#---------------------------------------#
+TEST_llnms_print_scanner_info_01(){
+    
+    #  make sure the scanner file exists
+    if [ ! -e $LLNMS_HOME/scanning/ping-scanner.llnms-scanner.xml ]; then
+        echo '1'
+        echo "Demo scanner at $LLNMS_HOME/scanning/ping-scanner.llnms-scanner.xml does not exist."
+        return
+    fi
+    SCANFILE="$LLNMS_HOME/scanning/ping-scanner.llnms-scanner.xml"
+    
+    #  Query info about the scanner
+    OUT="`llnms-print-scanner-info.sh -f $SCANFILE -i`"
+    if [ ! "$OUT" = "ping-scanner" ]; then echo '1'; echo "ID of scanner should be 'ping-scanner', actual answer was $OUT,  File: `basename $0`, Line: $LINENO." > /var/tmp/cause.txt; fi
+    
+    OUT="`llnms-print-scanner-info.sh -f $SCANFILE -n`"
+    if [ ! "$OUT" = "Ping Scanner" ]; then echo '1'; echo "Name of scanner should be 'Ping Scanner', actual answer was $OUT,  File: `basename $0`, Line: $LINENO." > /var/tmp/cause.txt; fi
+    
+    OUT="`llnms-print-scanner-info.sh -f $SCANFILE -c`"
+    if [ ! "$OUT" = "ping-scanner.bash" ]; then echo '1'; echo "Command for scanner should be 'ping-scanner.bash', actual answer was $OUT,  File: `basename $0`, Line: $LINENO." > /var/tmp/cause.txt; fi
+
+    echo '0'
+}
+
+
