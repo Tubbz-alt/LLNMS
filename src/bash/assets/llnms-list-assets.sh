96897bcf8694f76a830257b8b3cdff806c165405
Marvin Smith <marvin_smith1@me.com>
2013-12-26 16:57:10 -0800

    LLNMS List Asset lists assets accordingly and unit tests confirm this

--- src/bash/assets/llnms-list-assets.sh
+++ src/bash/assets/llnms-list-assets.sh
@@ -4,30 +4,78 @@
 #   Author:  Marvin Smith
 #   Date:    12/10/2013
 #
-
+#   Purpose:  Returns a list of LLNMS assets.
+#
 
 #----------------------------------------#
 #-       Print Usage Instructions       -#
 #----------------------------------------#
 usage(){
 
-    echo "$0 [options]"
+    echo "`basename $0` [options]"
     echo ''
     echo '    options:'
     echo '        -h, --help    :  Print usage instructions'
     echo '        -v, --version :  Print version information'
     echo ''
-    echo '        Formatting Options'
+    echo '        Formatting Options (Select only ONE of the following)'
     echo '        -p, --pretty  :  Print in human-readible format (Default)'
+    echo '        -l, --list    :  Print in list format.'
+    echo ''
+    echo '        Data to print (Note:  One or more combinations is acceptable)'
+    echo '        -a, --all            :  Print all asset information. (Default)'
+    echo '        -host, --host        :  Print hostname.'
+    echo '        -ip4, --ip4-address  :  Print the IP4 Address.'
+    echo '        -d, --description    :  Print the description.'
+    echo '        -s, --scanners       :  Print registered scanners.'
+    echo '        -path, --pathname    :  Print full pathname.'
     echo ''
 }
 
+#-------------------------------------#
+#-         Warning Function          -#
+#-                                   -#
+#-   $1 -  Error Message             -#
+#-   $2 -  Line Number (Optional).   -#
+#-   $3 -  File Name (Optional).     -$
+#-------------------------------------#
+warning(){
+
+    #  If the user only gives the warning message
+    if [ $# -eq 1 ]; then
+        echo "warning: $1"
+
+    #  If the user only gives the line number
+    elif [ $# -eq 2 ]; then
+        echo "warning: $1  Line: $2,  File: `basename $0`"
+
+    #  If the user gives the line number and file
+    else
+        echo "warning: $1  Line: $2, File: $3"
+    fi
+}
 
 #-------------------------------------#
-#             Error Function          #
+#-            Error Function         -#
+#-                                   -#
+#-   $1 -  Error Message             -#
+#-   $2 -  Line Number (Optional).   -#
+#-   $3 -  File Name (Optional).     -$
 #-------------------------------------#
 error(){
-    echo "error $1"
+
+    #  If the user only gives the error message
+    if [ $# -eq 1 ]; then
+        echo "error: $1"
+
+    #  If the user only gives the line number
+    elif [ $# -eq 2 ]; then
+        echo "error: $1  Line: $2,  File: `basename $0`"
+
+    #  If the user gives the line number and file
+    else
+        echo "error: $1  Line: $2, File: $3"
+    fi
 }
 
 
@@ -36,7 +84,7 @@ error(){
 #-------------------------------------#
 version(){
 
-    echo "$0 Information"
+    echo "`basename $0` Information"
     echo ''
     echo "   LLNMS Version ${LLNMS_MAJOR}.${LLNMS_MINOR}.${LLNMS_SUBMINOR}"
 
@@ -54,15 +102,25 @@ fi
 
 
 #  Import the version info
-source $LLNMS_HOME/config/llnms-info.sh
+. $LLNMS_HOME/config/llnms-info.sh
 
-#  Import utilities
-source $LLNMS_HOME/bin/llnms-asset-utilities.sh
+#  Import configuration
+. $LLNMS_HOME/config/llnms-config.sh
+
 
 #  Output Format
-#  PRETTY : Pretty
+#  PRETTY : Pretty format
+#  LIST   : List format
 OUTPUT_FORMAT='PRETTY'
 
+
+#Print options
+PRINT_EVERYTHING=1
+PRINT_HOSTNAME=0
+PRINT_IP4ADDRESS=0
+PRINT_DESCRIPTION=0
+PRINT_SCANNERS=0
+PRINT_PATHNAME=0
 
 #   Parse Command-Line Options
 for OPTION in $@; do
@@ -87,9 +145,49 @@ for OPTION in $@; do
             OUTPUT_FORMAT='PRETTY'
             ;;
 
+        #  Set List format
+        '-l' | '--list' )
+            OUTPUT_FORMAT='LIST'
+            ;;
+        
+        #  Print everything
+        '-a' | '--all' )
+            PRINT_EVERYTHING=1
+            ;;
+
+        #  Print hostname
+        '-host' | '--hostname' )
+            PRINT_EVERYTHING=0
+            PRINT_HOSTNAME=1
+            ;;
+
+        #  Print ip4 address
+        '-ip4' | '--ip4-address' )
+            PRINT_EVERYTHING=0
+            PRINT_IP4ADDRESS=1
+            ;;
+        
+        #  Print description
+        '-d' | '--description' )
+            PRINT_EVERYTHING=0
+            PRINT_DESCRIPTION=1
+            ;;
+
+        #  Print scanners
+        '-s' | '--scanners' )
+            PRINT_EVERYTHING=0
+            PRINT_SCANNERS=1
+            ;;
+
+        #  Print pathname
+        '-path' | '--pathname' )
+            PRINT_EVERYTHING=0
+            PRINT_PATHNAME=1
+            ;;
+
         #  Print error message
         *)
-            error "Unknown option $OPTION"
+            error "Unknown option $OPTION" "$LINENO"
             usage
             exit 1
             ;;
@@ -108,13 +206,74 @@ fi
 
 
 #  Get a list of assets in the asset folder
-ASSET_LIST=$(ls $LLNMS_HOME/assets/*.llnms-asset.xml 2> /dev/null)
+ASSET_LIST=`ls $LLNMS_HOME/assets/*.llnms-asset.xml 2> /dev/null`
 for ASSET_FILE in $ASSET_LIST; do
     
-    #  Print the asset hostname
-    echo "- Asset  hostname  : $(llnms_get_asset_hostname $ASSET_FILE)"
-    echo "        ip4 address: $(llnms_get_asset_ip4_address $ASSET_FILE)"
-    echo ''
+
+    DATA_PRINTED=0
+    #  If we are in pretty format, print header
+    if [ "$OUTPUT_FORMAT" = 'PRETTY' ]; then
+        echo "- Asset,  Path: `basename $ASSET_FILE`"
+    fi
+
+    #  Print pathname
+    if [ "$PRINT_EVERYTHING" = '1' -o "$PRINT_PATHNAME" = '1' ]; then
+        if [ "$OUTPUT_FORMAT" = "LIST" ]; then
+            echo "$ASSET_FILE\c"
+            DATA_PRINTED=1
+        fi
+    fi
+
+    #  Print Hostname
+    if [ "$PRINT_EVERYTHING" = "1" -o "$PRINT_HOSTNAME" = "1" ]; then
+        if [ "$OUTPUT_FORMAT" = "PRETTY" ]; then
+            echo "      Hostname: `llnms-print-asset-info.sh -host -f $ASSET_FILE`"
+        elif [ "$OUTPUT_FORMAT" = "LIST" ]; then
+            if [ "$DATA_PRINTED" = "1" ]; then
+                echo " \c"
+            fi
+            echo "`llnms-print-asset-info.sh -host -f $ASSET_FILE`\c"
+            DATA_PRINTED=1
+        fi
+    fi
+
+
+    #  Print IP4 Address
+    if [ "$PRINT_EVERYTHING" = '1' -o "$PRINT_IP4ADDRESS" = '1' ]; then
+        if [ "$OUTPUT_FORMAT" = "PRETTY" ]; then
+            echo "      IP4 Address: `llnms-print-asset-info.sh -ip4 -f $ASSET_FILE`"
+        elif [ "$OUTPUT_FORMAT" = "LIST" ]; then
+            if [ "$DATA_PRINTED" = "1" ]; then
+                echo " \c"
+            fi
+            echo "`llnms-print-asset-info.sh -ip4 -f $ASSET_FILE`\c"
+            DATA_PRINTED=1
+        fi
+    fi
+    
+    
+    #  Print description
+    if [ "$PRINT_EVERYTHING" = "1" -o "$PRINT_DESCRIPTION" = '1' ]; then
+        if [ "$OUTPUT_FORMAT" = "PRETTY" ]; then
+            echo "      Description: `llnms-print-asset-info.sh -d -f $ASSET_FILE`"
+        elif [ "$OUTPUT_FORMAT" = "LIST" ]; then    
+            if [ "$DATA_PRINTED" = '1' ]; then
+                echo " \c"
+            fi
+            echo "\"`llnms-print-asset-info.sh -d -f $ASSET_FILE`\"\c"
+            DATA_PRINTED=1
+        fi
+    fi
+
+    #  If we are done with the asset and in list output format, then start a new line
+    if [ "$OUTPUT_FORMAT" = "LIST" ]; then
+        echo ''
+    fi
+
+    #  Otherwise, if we are in prett format, create a new line
+    if [ "$OUTPUT_FORMAT" = "PRETTY" ]; then
+        echo ''
+    fi
 
 done
 
---
+++ test/assets/TEST_llnms_list_assets.sh
@@ -0,0 +1,129 @@
+#!/bin/sh
+#
+#  File:    TEST_llnms_list_assets.sh
+#  Author:  Marvin Smith
+#  Date:    12/26/2013
+#
+#  Purpose:  Performs all unit tests on the llnms-list-assets.sh script.
+#
+
+#  Make sure LLNMS has been installed
+if [ "$LLNMS_HOME" = "" ]; then
+    LLNMS_HOME="/var/tmp/llnms"
+fi
+
+#  Import llnms configuration
+. $LLNMS_HOME/config/llnms-config.sh
+
+# Initialize ANSI
+. test/unit_test/unit_test_utilities.sh
+
+
+#---------------------------------------------#
+#-     TEST_llnms_list_assets_01             -#
+#-                                           -#
+#-     First unit test of the llnms list     -#
+#-     assets function.                      -#
+#---------------------------------------------#
+TEST_llnms_list_assets_01(){
+
+    #  Make sure no assets exist in the directory
+    rm $LLNMS_HOME/assets/*.llnms-asset.xml 2> /dev/null
+
+    #  Create an asset using the create asset command
+    llnms-create-asset.sh  -host 'temp-asset1' -ip4 '192.168.0.1' -d 'hello world'
+    llnms-create-asset.sh  -host 'temp-asset2' -ip4 '10.2.18.200' -d 'other test file'
+    llnms-create-asset.sh  -host 'temp-asset3' -ip4 '172.2.18.200' -d 'final test file'
+    
+    #   Get a list of assets by name 
+    ASSETS="`llnms-list-assets.sh -l -path`"
+    A1=0
+    A2=0
+    A3=0
+    for ASSET in $ASSETS; do
+        case $ASSET in 
+            "$LLNMS_HOME/assets/temp-asset1.llnms-asset.xml" )
+                A1=1
+                ;;
+            "$LLNMS_HOME/assets/temp-asset2.llnms-asset.xml" )
+                A2=1
+                ;;
+            "$LLNMS_HOME/assets/temp-asset3.llnms-asset.xml" )
+                A3=1
+                ;;
+            *)
+                echo "Unknown Asset ($ASSET) found which does not match created assets.  File: `basename $0`, Line: $LINENO." > /var/tmp/cause.txt
+                echo '1'
+                return;
+                ;;
+
+            esac
+    done
+    if [ "$A1" = '0' ]; then echo "temp-asset1 was not listed when it should be. File `basename $0`, Line $LINENO." > /var/tmp/cause.txt; echo 1; return; fi
+    if [ "$A2" = '0' ]; then echo "temp-asset2 was not listed when it should be. File `basename $0`, Line $LINENO." > /var/tmp/cause.txt; echo 1; return; fi
+    if [ "$A3" = '0' ]; then echo "temp-asset3 was not listed when it should be. File `basename $0`, Line $LINENO." > /var/tmp/cause.txt; echo 1; return; fi
+    
+    #   Get a list of assets by name 
+    ASSETS="`llnms-list-assets.sh -l -host`"
+    A1=0
+    A2=0
+    A3=0
+    for ASSET in $ASSETS; do
+        case $ASSET in 
+            "temp-asset1" )
+                A1=1
+                ;;
+            "temp-asset2" )
+                A2=1
+                ;;
+            "temp-asset3" )
+                A3=1
+                ;;
+            *)
+                echo "Unknown Asset $ASSET found which does not match created assets.  File: `basename $0`, Line: $LINENO." > /var/tmp/cause.txt
+                echo '1'
+                return;
+            esac
+    done
+    if [ "$A1" = '0' ]; then echo "temp-asset1 was not listed when it should be. File `basename $0`, Line $LINENO." > /var/tmp/cause.txt; echo 1; return; fi
+    if [ "$A2" = '0' ]; then echo "temp-asset2 was not listed when it should be. File `basename $0`, Line $LINENO." > /var/tmp/cause.txt; echo 1; return; fi
+    if [ "$A3" = '0' ]; then echo "temp-asset3 was not listed when it should be. File `basename $0`, Line $LINENO." > /var/tmp/cause.txt; echo 1; return; fi
+
+    #   Get a list of assets by name 
+    ASSETS="`llnms-list-assets.sh -l -ip4`"
+    A1=0
+    A2=0
+    A3=0
+    for ASSET in $ASSETS; do
+        case $ASSET in 
+            "192.168.0.1" )
+                A1=1
+                ;;
+            "10.2.18.200" )
+                A2=1
+                ;;
+            "172.2.18.200" )
+                A3=1
+                ;;
+            *)
+                echo "Unknown Asset $ASSET found which does not match created assets.  File: `basename $0`, Line: $LINENO." > /var/tmp/cause.txt
+                echo '1'
+                return;
+            esac
+    done
+    if [ "$A1" = '0' ]; then echo "temp-asset1 was not listed when it should be. File `basename $0`, Line $LINENO." > /var/tmp/cause.txt; echo 1; return; fi
+    if [ "$A2" = '0' ]; then echo "temp-asset2 was not listed when it should be. File `basename $0`, Line $LINENO." > /var/tmp/cause.txt; echo 1; return; fi
+    if [ "$A3" = '0' ]; then echo "temp-asset3 was not listed when it should be. File `basename $0`, Line $LINENO." > /var/tmp/cause.txt; echo 1; return; fi
+
+
+
+    #  Remove all files
+    llnms-remove-asset.sh -host 'temp-asset1'
+    llnms-remove-asset.sh -host 'temp-asset2'
+    llnms-remove-asset.sh -host 'temp-asset3'
+
+    #  Successful Operation 
+    echo '0'
+
+}
+
