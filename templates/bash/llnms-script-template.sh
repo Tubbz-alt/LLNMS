301a9023176734592fd3739ec59a867569e814d6
Marvin Smith <marvin_smith1@me.com>
2014-01-20 22:48:04 -0800

    Removing .sh from llnms-config and llnms-options files

--- templates/bash/llnms-script-template.sh
+++ templates/bash/llnms-script-template.sh
@@ -90,10 +90,10 @@ fi
 
 
 #  Import the version info
-. $LLNMS_HOME/config/llnms-info.sh
+. $LLNMS_HOME/config/llnms-info
 
 #  Import the configuration info
-. $LLNMS_HOME/config/llnms-config.sh
+. $LLNMS_HOME/config/llnms-config
 
 #   Parse Command-Line Options
 for OPTION in "$@"; do
