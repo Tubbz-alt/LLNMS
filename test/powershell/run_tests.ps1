be5c216efe481bda2d7f89af13b5e87f51e4307b
Marvin Smith <marvin_smith1@me.com>
2014-01-27 19:30:33 -0800

    adding help/usage to powershell unit test suite

--- test/powershell/run_tests.ps1
+++ test/powershell/run_tests.ps1
@@ -4,3 +4,33 @@
 #
 #    Purpose:  Perform unit tests on the LLNMS Powershell Modules
 #
+Param( 
+    [parameter(Mandatory=$false)][switch]$Help
+)
+
+#-----------------------------------#
+#-          Usage Function         -#
+#-----------------------------------#
+Function usage(){
+    
+    Write-Host 'run-tests.ps1 [options]'
+    Write-Host ''
+    Write-Host '     options:'
+    Write-Host ''
+    Write-Host '      -Help   :  Print usage instructions'
+    Write-Host ''
+
+}
+
+
+
+
+#------------------------------------#
+#-            Main Program          -#
+#------------------------------------#
+
+#  Check if help has been requested
+if( $Help -eq $true ){
+    usage
+    exit 1
+}
