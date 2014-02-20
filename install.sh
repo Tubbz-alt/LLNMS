6f43698b86f9d679bf876a1eca2c7a0ef647c0b8
Marvin Smith <marvin_smith1@me.com>
2014-02-19 19:33:47 -0800

    adding file stub for install.sh.  I really need a single unified installer.

---
+++ install.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+#
+#    File:    install.sh
+#    Author:  Marvin Smith
+#    Date:    2/19/2014
+#
+#    Purpose:  Unified installer script for the LLNMS suite
+#
+
+
+
+
+
+
+for OPTION in "$@"; do
+
+
+done
+
+
