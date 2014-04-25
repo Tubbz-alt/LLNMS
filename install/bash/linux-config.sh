232c35be2ed8b688d7c414442c12b3a078e24e40
Marvin Smith <marvin_smith1@me.com>
2014-04-25 08:39:43 -0700

    Fixed the install script for the GUI by removing the static dependency on Boost.

--- install/bash/linux-config.sh
+++ install/bash/linux-config.sh
@@ -6,4 +6,6 @@ LLNMS_HOME="$1"
 #  Set echo
 echo "ECHO=echo" >> $LLNMS_HOME/config/llnms-config
 
+#  Set the number of threads
+echo "export LLNMS_THREAD_COUNT=`nproc`" >> $LLNMS_HOME/config/llnms-config
 
