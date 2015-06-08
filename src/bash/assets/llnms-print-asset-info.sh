dfa0bbf1d6aef308e90ee28292cf56e00198b47c
Marvin Smith <marvin_smith1@me.com>
2015-06-07 21:47:03 -0700

    Updated the print asset info section.

--- src/bash/assets/llnms-print-asset-info.sh
+++ src/bash/assets/llnms-print-asset-info.sh
@@ -22,7 +22,7 @@ usage(){
     echo '        -v, --version :  Print version information'
     echo '        -f, --file [ *.llnms-asset.xml File] : Select asset file.'
     echo ''
-    echo '    Information (Select only any combination of the following.  Note that multiple selections will be space-deliminated.)'
+    echo '    Information (Select any combination of the following.  Note that multiple selections will be space-deliminated.)'
     echo '        -a, --all             : Print everything (Default).'
     echo '        -host, --hostname     : Print the hostname'
     echo '        -ip4,  --ip4-address  : Print the IP4 Address.'
@@ -32,6 +32,10 @@ usage(){
     echo '        -sac, --scanner-arg-cnt   [scanner-id]              : Print the number of arguments given the scanner id and the scanner-argument number.'
     echo '        -san, --scanner-arg-name  [scanner-id] [arg-number] : Print the scanner argument name given the scanner id and the scanner-argument number.'
     echo '        -sav, --scanner-arg-value [scanner-id] [arg-number] : Print the scanner argument value given the scanner id and the scanner-argument number.'
+    echo ''
+    echo '    Formatting Options:'
+    echo '        -list   : Print in list format (space-delimited).'
+    echo '        -pretty : Print in pretty format.'
     echo ''
 }
 
@@ -94,6 +98,134 @@ version(){
 }
 
 
+#--------------------------------#
+#-      Get the Scanner ID      -#
+#--------------------------------#
+Get_Scanner_ID()
+{
+    #  find the index of the scanner id
+    ID_LIST=`xmlstarlet sel -t -m '//llnms-asset/scanners/scanner' -n -v 'id' $FILE_VALUE | sed '/^\s*$/d' | sed 's/ *//g'`
+
+    #  Iterate over scanners, checking if the requested id matches
+    TEMP_IDX=1
+    ACTUAL_IDX=0
+    for ID in $ID_LIST; do
+        
+        #  If the requested ID matches, set the index
+        if [ "$ID" = "$1" ]; then
+            ACTUAL_IDX=$TEMP_IDX
+
+        #  Increment the counter
+        else
+            TEMP_IDX=`expr $TEMP_IDX + 1` 
+        fi
+    done
+
+    echo "$ACTUAL_IDX"
+}
+
+#-------------------------------------------#
+#-      Get the Scanner Argument Value     -#
+#-------------------------------------------#
+Get_Scanner_Argument_Value()
+{
+    #  Get the ID
+    IDX="`Get_Scanner_ID $1`"
+
+    # Get the Arg Number
+    ARG_NO=$2
+    
+    #  Return the value
+    echo "`xmlstarlet sel -t -m //llnms-asset/scanners/scanner[$IDX]/argument[$ARG_NO] -n -v @value $FILE_VALUE | sed '/^\s*$/d' `"
+
+}
+
+
+#------------------------------------------#
+#-      Get the Scanner Argument Name     -#
+#------------------------------------------#
+Get_Scanner_Argument_Name()
+{
+    #  Get the ID
+    IDX="`Get_Scanner_ID $1`"
+
+    # Get the Arg Number
+    ARG_NO=$2
+    
+    #  Return the value
+    echo "`xmlstarlet sel -t -m //llnms-asset/scanners/scanner[$IDX]/argument[$ARG_NO] -n -v @name $FILE_VALUE | sed '/^\s*$/d' | sed 's/ *//g'` "
+}
+
+
+#------------------------------------------#
+#-     Get the Scanner Argument Count     -#
+#------------------------------------------#
+Get_Scanner_Argument_Count()
+{
+    #  Get the ID
+    IDX="`Get_Scanner_ID $1`"
+
+    #  Get the value of the argument
+    echo "`xmlstarlet sel -t -m //llnms-asset/scanners/scanner[$IDX]/argument -n -v @value $FILE_VALUE | sed '/^\s*$/d' | wc -l | sed 's/ *//g'`"
+}
+
+
+#------------------------#
+#-      Print Data      -#
+#------------------------#
+Print_Data()
+{
+    #  Get the format
+    if [ "$FORMAT" = 'pretty' ]; then
+        printf "%12s:  %-12s\n" "$2" "$1"
+    else
+
+        #  Add space for gap
+        if [ "$DATA_PRINTED" = '1' ]; then
+            printf " "
+        fi
+
+        #  Print content
+        printf "$1"
+    fi
+}
+
+
+#--------------------------------#
+#-      Print Scanner Data      -#
+#--------------------------------#
+Print_Scanner_Data()
+{
+    #  Get the arguments
+    IDX=$1
+    SCANNER_NAME=$2
+    ARG_CNT=$3
+
+    #  Get the format
+    if [ "$FORMAT" = 'pretty' ]; then
+        #  Print the name and id
+        printf  "%12s:  %-29s\n"  "Scanner" "$1"
+        printf  "%20s  %-14s\n"  "-name    " "$2"
+        
+        # Iterate over arguments
+        for ((X=1;X<=${ARG_CNT};X++)); do
+            printf "%20s  %s" "-argument" "${ARG_NAMES[$X]}" 
+            printf "  value: %-20s\n" "${ARG_VALUES[$X]}" 
+        done
+
+    else
+
+        #  Add space for gap
+        if [ "$DATA_PRINTED" = '1' ]; then
+            printf " "
+        fi
+
+        #  Print content
+        printf "$1"
+    fi
+}
+
+
 #---------------------------------#
 #-         Main Function         -#
 #---------------------------------#
@@ -129,6 +261,8 @@ SCANNER_ARG_ID_FLAG=0
 SCANNER_ARG_NO_FLAG=0
 SCANNER_ARG_ID=''
 SCANNER_ARG_NO=''
+
+FORMAT='list'
 
 
 #   Parse Command-Line Options
@@ -200,6 +334,16 @@ for OPTION in "$@"; do
             EVERYTHING_FLAG=1
             ;;
 
+        #  Set format to list
+        '-list')
+            FORMAT='list'
+            ;;
+
+        #  Set format to pretty
+        '-pretty')
+            FORMAT='pretty'
+            ;;
+
         #  Process flag values or print error message
         *)
             
@@ -268,105 +412,94 @@ fi
 #----------------------------------------------#
 DATA_PRINTED=0
 
+
 #  Hostname
 if [ "$HOSTNAME_FLAG" = '1' -o "$EVERYTHING_FLAG" = '1' ]; then
-    printf "`xmlstarlet sel -t -m '//llnms-asset' -v 'hostname' -n $FILE_VALUE`"
+
+    #  Grab the hostname
+    PRINT_INFO="`xmlstarlet sel -t -m '//llnms-asset' -v 'hostname' -n $FILE_VALUE`"
+
+    #  Print the output
+    Print_Data $PRINT_INFO 'Hostname'
+
     DATA_PRINTED=1
 fi
+
 
 #  IP4 Address
 if [ "$IP4ADDRESS_FLAG" = '1' -o "$EVERYTHING_FLAG" = '1' ]; then
 
-    if [ "$DATA_PRINTED" = '1' ]; then
-        printf " "
-    fi
-
-    printf "`xmlstarlet sel -t -m '//llnms-asset' -v 'ip4-address' -n $FILE_VALUE`"
+    # Grab the IP4 Address
+    PRINT_INFO="`xmlstarlet sel -t -m '//llnms-asset' -v 'ip4-address' -n $FILE_VALUE`"
+    Print_Data $PRINT_INFO 'ip4-address'
     DATA_PRINTED=1
 fi
+
 
 #  Description
 if [ "$DESCRIPTION_FLAG" = '1' -o "$EVERYTHING_FLAG" = '1' ]; then
-    if [ "$DATA_PRINTED" = '1' ]; then
-        printf " "
-    fi
-
-    printf "`xmlstarlet sel -t -m '//llnms-asset' -v 'description' -n $FILE_VALUE`"
+    PRINT_INFO="`xmlstarlet sel -t -m '//llnms-asset' -v 'description' -n $FILE_VALUE`"
+    Print_Data $PRINT_INFO 'Description'
     DATA_PRINTED=1
+fi
+
+#  Scanners for everything flag
+if [ "$EVERYTHING_FLAG" = '1' ]; then
+    
+    # Get the scanner list
+    SCANNER_NAMES="`xmlstarlet sel -t -m '//llnms-asset/scanners/scanner' -n -v '.' -n $FILE_VALUE | sed 's/ *//g' | sed '/^\s*$/d'`"
+
+    #  Iterate
+    for SCANNER_NAME in $SCANNER_NAMES; do
+        
+        #  Get the ID
+        SCANNER_ID="`Get_Scanner_ID $SCANNER_NAME`"
+
+        #  Get the Argument Count
+        ARG_CNT=`Get_Scanner_Argument_Count $SCANNER_NAME`
+        
+        #  Iterate over each argument
+        for ((X=1;X<=${ARG_CNT};X++)); do
+        
+            #  Get the argument name
+            ARG_NAMES[$X]=`Get_Scanner_Argument_Name   $SCANNER_NAME  $X`
+            ARG_VALUES[$X]=`Get_Scanner_Argument_Value $SCANNER_NAME  $X`
+        done
+
+        # Print
+        Print_Scanner_Data $SCANNER_ID $SCANNER_NAME $ARG_CNT
+    done
 fi
 
 #  Scanners
-if [ "$SCANNER_FLAG" = '1' -o "$EVERYTHING_FLAG" = '1' ]; then
-    if [ "$DATA_PRINTED" = '1' ]; then
-        printf " "
-    fi
-    printf "`xmlstarlet sel -t -m '//llnms-asset/scanners/scanner' -n -v '.' -n $FILE_VALUE | sed 's/ *//g' | sed '/^\s*$/d'`"
+if [ "$SCANNER_FLAG" = '1' ]; then
+
+    PRINT_INFO="`xmlstarlet sel -t -m '//llnms-asset/scanners/scanner' -n -v '.' -n $FILE_VALUE | sed 's/ *//g' | sed '/^\s*$/d'`"
+    Print_Data $PRINT_INFO 'Scanners'
     DATA_PRINTED=1
 fi
 
-#  Scanner arguments
+#  Scanner argument name
 if [ "$SCANNER_ARG_NAME_FLAG" = '1' ]; then
-    
-    #  find the index of the scanner id
-    ID_LIST=`xmlstarlet sel -t -m '//llnms-asset/scanners/scanner' -n -v 'id' $FILE_VALUE | sed '/^\s*$/d' | sed 's/ *//g'`
-
-    TEMP_IDX=1
-    ACTUAL_IDX=0
-    for ID in $ID_LIST; do
-        if [ "$ID" = "$SCANNER_ARG_ID" ]; then
-            ACTUAL_IDX=$TEMP_IDX
-        else
-            TEMP_IDX=`expr $TEMP_IDX + 1` 
-        fi
-    done
-    
-    #  Get the value of the argument
-    echo "`xmlstarlet sel -t -m //llnms-asset/scanners/scanner[$ACTUAL_IDX]/argument[$SCANNER_ARG_NO] -n -v @name $FILE_VALUE | sed '/^\s*$/d' | sed 's/ *//g'` "
-
+    PRINT_INFO=`Get_Scanner_Argument_Name $SCANNER_ARG_ID $SCANNER_ARG_NO`
+    Print_Data $PRINT_INFO 'Scanner Argument Name'
+    DATA_PRINTED=1
 fi
 
+
+#  Scanner Argument Value
 if [ "$SCANNER_ARG_VALUE_FLAG" = '1' ]; then
-    
-    #  find the index of the scanner id
-    ID_LIST=`xmlstarlet sel -t -m '//llnms-asset/scanners/scanner' -n -v 'id' $FILE_VALUE | sed '/^\s*$/d' | sed 's/ *//g'`
-
-    TEMP_IDX=1
-    ACTUAL_IDX=0
-    for ID in $ID_LIST; do
-        if [ "$ID" = "$SCANNER_ARG_ID" ]; then
-            ACTUAL_IDX=$TEMP_IDX
-        else
-            TEMP_IDX=`expr $TEMP_IDX + 1` 
-        fi
-    done
-    
-    #  Get the value of the argument
-    echo "`xmlstarlet sel -t -m //llnms-asset/scanners/scanner[$ACTUAL_IDX]/argument[$SCANNER_ARG_NO] -n -v @value $FILE_VALUE | sed '/^\s*$/d' `"
-    exit 0
-
+    PRINT_INFO=`Get_Scanner_Argument_Value $SCANNER_ARG_ID $SCANNER_ARG_NO`
+    Print_Data $PRINT_INFO 'Scanner Argument Value'
+    DATA_PRINTED=1
 fi
 
+#  Scanner ARgument Count
 if [ "$SCANNER_ARG_COUNT_FLAG" = '1' ]; then
-    
-    #  find the index of the scanner id
-    ID_LIST=`xmlstarlet sel -t -m '//llnms-asset/scanners/scanner' -n -v 'id' $FILE_VALUE | sed '/^\s*$/d' | sed 's/ *//g'`
-
-    TEMP_IDX=1
-    ACTUAL_IDX=0
-    for ID in $ID_LIST; do
-        if [ "$ID" = "$SCANNER_ARG_ID" ]; then
-            ACTUAL_IDX=$TEMP_IDX
-        else
-            TEMP_IDX=`expr $TEMP_IDX + 1` 
-        fi
-    done
-    
-    #  Get the value of the argument
-    echo "`xmlstarlet sel -t -m //llnms-asset/scanners/scanner[$ACTUAL_IDX]/argument -n -v @value $FILE_VALUE | sed '/^\s*$/d' | wc -l | sed 's/ *//g'`"
-    exit 0
-
+    PRINT_INFO=`Get_Scanner_Argument_Count $SCANNER_ARG_ID`
+    Print_Data $PRINT_INFO 'Scanner Argument Count'
+    DATA_PRINTED=1
 fi
-
 
 
 echo ''
